3756d95078d5f8816f1618e7a3e788caf664912b
Micinova <micinova@abv.bg>
2015-05-20 19:12:11 +0300

    [C#Advanced]

---
+++ [C# Advanced]/Other/StringBuilder-SubstringMethod/StringBuilderExtentsion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+   public static class StringBuilderExtentsion
+    {
+       public static string Substring(this StringBuilder sb, int startIndex, int count)
+       {
+
+           StringBuilder das = new StringBuilder();
+           for (int i = sb[startIndex]; i < sb[count]; i++)
+           {
+               das.Append((char)i);
+               
+           }
+           //string result = das.ToString();
+           //Console.WriteLine(result);
+           return das.ToString();
+       }
+    }
+}
---
+++ [C# Advanced]/Other/StringBuilder-SubstringMethod/sbSubstring.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class sbSubstring
+    {
+        static public void Main(string[] args)
+        {
+           StringBuilder alphabet = new StringBuilder();
+
+            for (int i = 'a'; i < 'z'; i++)
+            {
+                alphabet.Append((char) i); 
+            }
+
+            string firstTen = alphabet.Substring(0, 10);
+
+            Console.WriteLine(firstTen);
+            
+        }
+    }
+}
