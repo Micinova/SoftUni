1cc67fd00e01d89fa090df876b387197a9483e6e
Micinova <MicinovaD@gmail.com>
2015-06-18 15:58:23 +0300

    [C# OOP]

---
+++ [C# OOP]/RestaurantManager-Skeleton/RestaurantManagerProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace RestaurantManager
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+    using RestaurantManager.Engine;
+
+    public class RestaurantManagementProgram
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            RestaurantManagerEngine.Instance.Start();
+
+            Drink ajran = new Drink("ajran", 1m, 87, 300, 2, false);
+            Console.WriteLine(ajran.Unit);
+        }
+    }
+}
